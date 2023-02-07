94c05f86e86e9b75b423a8884d6b15f8acb98cae
Parthe Pandit <parthe@users.noreply.github.com>
2023-02-06 20:25:17 -0800

    Update dataset.py

--- eigenpro3/dataset.py
+++ eigenpro3/dataset.py
@@ -4,8 +4,9 @@ from .svd import nystrom_kernel_svd
 
 class Dataset:
 
-    def __init__(self, data, kernel_fn, nystrom_size=10000, top_q=None, precondition=False):
-        self.data = data
+    def __init__(self, data, kernel_fn, nystrom_size=10000, top_q=None, precondition=False, device=None):
+        self.device = device
+        self.data = data.to(self.device)
         self.n_samples = len(data)
         self.kernel = kernel_fn
         self.nystrom_size = nystrom_size
@@ -18,11 +19,11 @@ class Dataset:
 
 
     def setup_preconditioner(self):
-        self.nystrom_ids = torch.randperm(self.n_samples)[:self.nystrom_size]
+        self.nystrom_ids = torch.randperm(self.n_samples)[:self.nystrom_size].to(self.device)
         eigvals, eigvecs, self.beta = nystrom_kernel_svd(
             self.data[self.nystrom_ids].cpu(), self.kernel, self.top_q+1)
-        self.scaled_eigvecs = eigvecs[:, 1:]*((1-eigvals[0]/eigvals[1:])*1/eigvals[1:]).sqrt()
-        self.tail_eigval = eigvals[0]
+        self.scaled_eigvecs = (eigvecs[:, 1:]*((1-eigvals[0]/eigvals[1:])*1/eigvals[1:]).sqrt()).to(self.device)
+        self.tail_eigval = eigvals[0].to(self.device)
 
 
     def corrector(self, g, batch_ids):
