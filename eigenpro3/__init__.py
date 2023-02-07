2f3a66b92c2a86476f6b637416ca831658e1b43a
Parthe Pandit <parthe1292@gmail.com>
2023-02-06 20:34:49 -0800

    removed old code files

--- eigenpro3/__init__.py
+++
@@ -1,4 +0,0 @@
-from .regressor import KernelModel
-from .dataset import Dataset
-
-__version__ = "3.0"
--- eigenpro3/utils.py
+++
@@ -1,3 +0,0 @@
-def get_optimal_params(mem_gb):
-    raise NotImplementedError
-    return bs, eta, top_q
