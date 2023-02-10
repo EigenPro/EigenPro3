fc472b63780505f86c44d013628d359d3c9d196d
Parthe Pandit <parthe1292@gmail.com>
2023-02-09 23:01:52 -0800

    aa

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from eigenpro3.models import KernelModel
 from eigenpro3.kernels import laplacian, ntk_relu
 from torch.nn.functional import one_hot
 
+DEVICE_LIST = [torch.device('cuda:0'), torch.device('cuda:1')] 
+
 p = 5000 # model size
 
 kernel_fn = lambda x, z: laplacian(x, z, bandwidth=20.0)
@@ -19,9 +21,7 @@ testloader = torch.utils.data.DataLoader(
     CustomDataset(X_test, y_test.argmax(-1)), batch_size=512,
     shuffle=False, num_workers=16,pin_memory=True)
 
-
 model = KernelModel(y_train, centers, kernel_fn, X=X_train,
-    devices =[torch.device('cuda:0'), torch.device('cuda:1')], 
-    multi_gpu=True)
+    devices = DEVICE_LIST, multi_gpu=True)
 
 model.fit(model.train_loaders, testloader, score_fn=accuracy)
