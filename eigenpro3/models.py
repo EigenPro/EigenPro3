f4787295bdf7843d21f8fdb45bb1fefe8ef86814
Amirhesam Abedsoltan <91305454+ahabedsoltan@users.noreply.github.com>
2023-03-03 11:18:09 -0800

    Update models.py

--- eigenpro3/models.py
+++ eigenpro3/models.py
@@ -1,22 +1,17 @@
 import torch, time
-from .utils import fmm, accuracy, divide_to_gpus, bottomrule, midrule,mse,log_performance
+from .utils import fmm, accuracy, divide_to_gpus, bottomrule, midrule 
 from .utils.svd import nystrom_kernel_svd
 from .datasets import makedataloaders
 from .projection import HilbertProjection
 from torch.cuda.comm import broadcast
 from concurrent.futures import ThreadPoolExecutor
 
-import ipdb
 
 class KernelModel():
 
     def __init__(self, n_classes ,centers, kernel_fn,y=None,X=None, devices =[torch.device('cpu')], make_dataloader=True,
-                 nystrom_samples=None, n_nystrom_samples=5_000,
-                 data_preconditioner_level=500,everyTProject=5,wandb_run=None):
-        self.wandb_run = wandb_run
+                 nystrom_samples=None, n_nystrom_samples=5_000, data_preconditioner_level=500):
 
-
-        self.T = everyTProject
         self.devices = tuple(devices)
         self.device_base = self.devices[0]
 
@@ -26,21 +21,18 @@ class KernelModel():
         self.centers = centers
         self.n_centers = len(centers)
         self.kernel = kernel_fn
-
         self.weights = torch.zeros(self.n_centers, n_classes)
-
-
-
-        # self.preconditoners_grad = []
-        self.corrected_gz_scaled = 0
 
         
         self.multi_gpu = len(self.devices) > 1
+
         if self.multi_gpu:
             ######## dsitributing the weights over all avalibale GPUs
             self.weights_all = divide_to_gpus(self.weights,self.n_centers, self.devices)
+
             ######## dsitributing the centers over all avalibale GPUs
             self.centers_all = divide_to_gpus(self.centers,self.n_centers, self.devices)
+
         else:
             self.weights_all = [self.weights.to(self.device_base)]
             self.centers_all = [self.centers.to(self.device_base)]
@@ -56,27 +48,17 @@ class KernelModel():
             self.nystrom_samples = nystrom_samples
         print('Setting up data preconditioner')
         start_time = time.time()
-        self.data_preconditioner_matrix, self.eigenvectors_data,self.D, self.batch_size,self.lr \
+        self.data_preconditioner_matrix, self.eigenvectors_data, self.batch_size,self.lr \
             = self.get_preconditioner(data_preconditioner_level + 1)
         print(f'Setup time = {time.time()-start_time:5.2f} s')
         print("Done.\n" + bottomrule)
-
-        self.tmp_centers = torch.zeros(self.batch_size * self.T,self.centers.shape[1])
-        self.weights_newcenters = torch.zeros(self.batch_size*self.T, n_classes)
-        self.weights_xs = torch.zeros(self.nystrom_samples.shape[0], n_classes).to(self.device_base)
 
         self.centers = self.centers.to(self.device_base)
         self.weights = self.weights.to(self.device_base)
         self.nystrom_samples = self.nystrom_samples.to(self.device_base)
         self.data_preconditioner_matrix = self.data_preconditioner_matrix.to(self.device_base)
         self.eigenvectors_data = self.eigenvectors_data.to(self.device_base)
-        self.D = self.D.to(self.device_base)
         ###### DATA Preconditioner #########
-
-        #####log into wandb#####
-        self.wandb_run.summary['learning rate'] = self.lr
-        self.wandb_run.summary['T'] = self.T
-        self.wandb_run.summary['batch_size'] = self.batch_size
 
 
 
@@ -92,16 +74,14 @@ class KernelModel():
         print('Done.\n' + midrule)
 
     def fit(self, train_loaders, val_loader=None,epochs=10,score_fn=None):
-        self.val_loader = val_loader
-        self.step = 0
         for t in range(epochs):
             self.epoch = t
             self.fit_epoch(train_loaders)
-            # if val_loader!=None and score_fn!=None:
-            #     log_performance(self.weights,self.centers,val_loader[0],
-            #                     self.kernel,self.device_base,self.wandb_run,t,name='Train')
-            #     log_performance(self.weights, self.centers, val_loader[1],
-            #                     self.kernel, self.device_base,self.wandb_run,t, name='Test')
+            if val_loader!=None and score_fn!=None:
+                accu = score_fn(self.weights,self.centers,val_loader,self.kernel,self.device_base)
+                print(midrule)
+                print(f'epoch {t+1:4d}        validation accuracy: {accu*100.:5.2f}%')
+                print(bottomrule)
 
 
 
@@ -110,7 +90,6 @@ class KernelModel():
         for trainloader_ind, train_loader in enumerate(train_loaders):
             for (X_batch, y_batch) in train_loader:
                 ###### fitting the batch
-
                 self.fit_batch(X_batch, y_batch)
 
                 if self.multi_gpu:
@@ -118,7 +97,7 @@ class KernelModel():
 
                 torch.cuda.empty_cache()
 
-                if (batch_num + 1)%1==0:
+                if (batch_num + 1)%2==0:
                     print(f'epoch {self.epoch: 4d}\t batch {batch_num+1 :4d}')
 
                 batch_num += 1
@@ -126,7 +105,7 @@ class KernelModel():
 
 
     def fit_batch(self, X_batch, y_batch):
-        # print('Batch Size:',X_batch.shape)
+
         if self.multi_gpu:
             ##### Putting a copy of the batch in all available GPUs
             X_batch_all = broadcast(X_batch, self.devices)
@@ -135,20 +114,10 @@ class KernelModel():
             X_batch_all = [X_batch.to(self.device_base)]
             y_batch_all = [y_batch.to(self.device_base)]
 
-
-        preconditoner_grad = self.precondition_correction(X_batch_all[0])
-        # self.preconditoners_grad.append(preconditoner_grad)
-
-
-
         gz, grad = self.get_gradient(X_batch_all, y_batch_all)
 
 
-        which_newnodes_start = (self.step%self.T)*self.batch_size
-        which_newnodes_end = (self.step % self.T + 1)*self.batch_size
-        self.tmp_centers[which_newnodes_start:which_newnodes_end] = X_batch_all[0]
-
-        preconditon_grad = self.data_preconditioner_matrix @  (preconditoner_grad @ grad)
+        preconditon_grad = self.precondition_correction(X_batch_all[0], grad)
         corrected_gz = gz - preconditon_grad
 
         del X_batch_all, y_batch_all
@@ -156,93 +125,48 @@ class KernelModel():
             self.sync_gpus()
 
         torch.cuda.empty_cache()
-        self.corrected_gz_scaled += corrected_gz.to(self.device_base)
-
-
-        if self.step%self.T==0:
-            print('Projection...')
-            self.update_weights()
-            self.tmp_centers = torch.zeros(self.batch_size*self.T, self.centers.shape[1]).to(self.device_base)
-            self.weights_newcenters = torch.zeros(self.batch_size*self.T, self.n_classes).to(self.device_base)
-            # self.preconditoners_grad = []
-            self.corrected_gz_scaled = 0
-            self.weights_xs = torch.zeros(self.nystrom_samples.shape[0], self.n_classes).to(self.device_base)
-            print('Projection done.')
-
-        print('performance...')
-
-        which_newnodes_end = (self.step % self.T + 1)*self.batch_size
-
-        if self.T%10==0:
-            log_performance([self.weights,self.weights_xs,self.weights_newcenters[:which_newnodes_end]],
-                            [self.centers,self.nystrom_samples,self.tmp_centers[:which_newnodes_end]],
-                            self.val_loader[0],self.kernel,self.devices,self.wandb_run,self.step,name='Train')
-            log_performance([self.weights,self.weights_xs,self.weights_newcenters[:which_newnodes_end]],
-                            [self.centers,self.nystrom_samples,self.tmp_centers[:which_newnodes_end]] ,
-                            self.val_loader[1],self.kernel, self.devices,self.wandb_run,self.step, name='Test')
-        self.step +=1
-
-
+        self.corrected_gz_scaled = corrected_gz.to(self.device_base)
+        self.update_weights()
 
 
     def get_gradient(self, X_batch_all, y_batch_all):
 
-        Kxbatch_xs = self.kernel(X_batch_all[0], self.nystrom_samples)
+        ####### Kz_xbatch calculation parallel on GPUs
+        with ThreadPoolExecutor() as executor:
+            Kz_xbatch_chunk = [executor.submit(self.kernel, inputs[0], inputs[1]) for inputs
+                    in zip(*[X_batch_all, self.centers_all])]
 
-        # Kxbatch_z = self.kernel(X_batch_all[0], self.centers)
-        # predict_z = Kxbatch_z @ self.weights
+        kxbatchz_all = [i.result() for i in Kz_xbatch_chunk]
 
-        predict_z,Kz_xbatch_chunk = self.multi_gpu_grad(X_batch_all, self.centers_all,
-                                                   self.weights_all,Kz_xbatch_chunk_return=True)
+        ######## gradient calculation parallel on GPUs
+        with ThreadPoolExecutor() as executor:
+            gradients = [executor.submit(fmm, inputs[0], inputs[1], inputs[2],self.device_base) for inputs
+                    in zip(*[kxbatchz_all, self.weights_all, y_batch_all])]
 
-        which_newnodes_start = (self.step % self.T) * self.batch_size
-        which_newnodes_end = (self.step % self.T + 1) * self.batch_size
+        del kxbatchz_all
 
-        if self.step%self.T==0:
-            print('here1')
-            grad = predict_z - y_batch_all[0]
-        else:
-            print('here2')
-            # self.centers_all = divide_to_gpus(self.centers, self.n_centers, self.devices)
-
-            # Kxbatch_newcenters = self.kernel(X_batch_all[0], self.tmp_centers[which_newnodes_start:which_newnodes_end])
-            # predict_newcenters = Kxbatch_newcenters@torch.cat(self.weights_newcenters)
-
-            tmp_centers_all = divide_to_gpus(self.tmp_centers[0:which_newnodes_start],
-                                             self.tmp_centers[0:which_newnodes_start].shape[0],
-                                             self.devices)
-            weights_newcenters_all = divide_to_gpus(self.weights_newcenters[0:which_newnodes_start],
-                                                    self.weights_newcenters[0:which_newnodes_start].shape[0],
-                                                    self.devices)
-            predict_newcenters = self.multi_gpu_grad(X_batch_all,tmp_centers_all,weights_newcenters_all)
-
-            predict_xs  = Kxbatch_xs@self.weights_xs
-            grad = predict_newcenters+predict_xs+predict_z -y_batch_all[0]
-
-        self.weights_xs = self.weights_xs +(self.lr / (self.batch_size)) *\
-                          (self.eigenvectors_data*self.D@(self.eigenvectors_data.T@(Kxbatch_xs.T@grad )))
-        # self.weights_newcenters.append( -(self.lr / (self.batch_size)) *grad )
-
-        self.weights_newcenters[which_newnodes_start:which_newnodes_end] = -(self.lr / (self.batch_size)) *grad
-
+        grad = 0
         out = []
+        ##### summing gradients over GPUs
+        for r in gradients:
+            grad += r.result()
+        del gradients
+        
+        ###### complete gradient
+        grad = grad - y_batch_all[0]
+
+        ##### gradients calculated in centers
         for r in Kz_xbatch_chunk:
             kgrad = r.result().T @ grad.to(r.result().device)
             out.append(kgrad.to(self.device_base))
         del Kz_xbatch_chunk, kgrad
-
+        
         if self.multi_gpu:
             self.sync_gpus()
         torch.cuda.empty_cache()
         gz = torch.cat(out, dim=0)
 
-        # gz = Kxbatch_z.T@grad
-
-
-        return gz,grad
-
-
-
+        return gz, grad
 
 
     def get_preconditioner(self, data_preconditioner_level):
@@ -259,7 +183,6 @@ class KernelModel():
         D_x = (1 - tail_eig_x / Lam_x) / Lam_x / nystrom_size
 
         batch_size = int(beta / tail_eig_x)
-
         if batch_size < beta / tail_eig_x + 1:
             lr = batch_size / beta / (2)
         else:
@@ -272,26 +195,22 @@ class KernelModel():
         Kmat_xs_z = self.kernel(self.nystrom_samples.cpu(), self.centers)
         preconditioner_matrix = Kmat_xs_z.T @ (D_x * E_x)
         del Kmat_xs_z
-        return preconditioner_matrix, E_x,D_x, batch_size, lr
+        return preconditioner_matrix, E_x, batch_size, lr
 
     
-    def precondition_correction(self, X_batch):
+    def precondition_correction(self, X_batch, grad):
         Kmat_xs_xbatch = self.kernel(self.nystrom_samples, X_batch)
-        # preconditoner_grad = self.data_preconditioner_matrix @ (self.eigenvectors_data.T @ (Kmat_xs_xbatch))
-        preconditoner_grad = self.eigenvectors_data.T @ (Kmat_xs_xbatch)
+        preconditon_grad = self.data_preconditioner_matrix @ (self.eigenvectors_data.T @ (Kmat_xs_xbatch @ grad))
         del Kmat_xs_xbatch
-        return preconditoner_grad
+        return preconditon_grad
 
 
     def update_weights(self):
 
         gz_projection = self.corrected_gz_scaled.to(self.device_base)
-        self.theta2 = self.inexact_projector.fit_hilbert_projection(
+        self.theta2= self.inexact_projector.fit_hilbert_projection(
             gz_projection, mem_gb=20)
-        # self.theta2 = torch.inverse(self.kernel(self.centers,self.centers))@gz_projection
-        # self.theta2 = torch.linalg.solve(self.kernel(self.centers,self.centers), gz_projection)
 
-        # print(self.theta2)
         new_weights = self.weights.to(self.device_base) - (self.lr / (self.batch_size)) * self.theta2.to(self.device_base)
         
         self.weights = new_weights.to(self.device_base)
@@ -310,30 +229,3 @@ class KernelModel():
     def sync_gpus(self):
         for i in self.devices:
             torch.cuda.synchronize(i)
-
-    def multi_gpu_grad(self,X_batch_all,centers_all,weights_all,Kz_xbatch_chunk_return=False):
-
-        with ThreadPoolExecutor() as executor:
-            Kz_xbatch_chunk = [executor.submit(self.kernel, inputs[0], inputs[1]) for inputs
-                               in zip(*[X_batch_all, centers_all])]
-
-        kxbatchz_all = [i.result() for i in Kz_xbatch_chunk]
-
-        ######## gradient calculation parallel on GPUs
-        with ThreadPoolExecutor() as executor:
-            gradients = [executor.submit(fmm, inputs[0], inputs[1], self.device_base) for inputs
-                         in zip(*[kxbatchz_all, weights_all])]
-
-        del kxbatchz_all
-
-        grad = 0
-        ##### summing gradients over GPUs
-        for r in gradients:
-            grad += r.result()
-        del gradients
-
-        if Kz_xbatch_chunk_return:
-            return grad,Kz_xbatch_chunk
-        else:
-            return grad
-
