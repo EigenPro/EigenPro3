bc9f0c815071f87c78f2d82e9f5bfe621fa5f043
Amirhesam Abedsoltan <91305454+ahabedsoltan@users.noreply.github.com>
2023-03-05 19:24:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from eigenpro3.kernels import laplacian, ntk_relu
 from torch.nn.functional import one_hot
 import os
 
-os.environ['DATA_DIR'] = '/expanse/lustre/projects/csd716/amirhesam/data/'  ##### add your data directory
+os.environ['DATA_DIR'] = ''  ##### add your data directory
+
 if torch.cuda.is_available():
-    DEVICE_LIST = (torch.device(f'cuda:{i}') for i in range(torch.cuda.device_count()))
+     DEVICE_LIST = (torch.device(f'cuda:{i}') for i in range(torch.cuda.device_count()))
 else:
-    DEVICE_LIST = [torch.device(f'cpu')]
+     DEVICE_LIST = [torch.device(f'cpu')]
+
 
 p = 5000 # model size
 
@@ -27,17 +29,6 @@ testloader = torch.utils.data.DataLoader(
     shuffle=False, pin_memory=True)
 
 
-<<<<<<< HEAD
-
-print(f'number of training set:{X_train.shape[0]}')
-print(f'number of centers:{centers.shape[0]}')
-
-model = KernelModel(n_classes, centers, kernel_fn, everyTProject=args.T,
-                    X=X_train,y=y_train,devices = DEVICE_LIST,wandb_run=wandb_run)
-
-model.fit(model.train_loaders, [trainloader,testloader], score_fn=accuracy, epochs=50)
-=======
 model = KernelModel(n_classes, centers, kernel_fn, X=X_train,y=y_train,devices = DEVICE_LIST)
 
 model.fit(model.train_loaders, testloader, score_fn=accuracy, epochs=20)
->>>>>>> 156247e8792167d75bd3068c71758a242590b87c
